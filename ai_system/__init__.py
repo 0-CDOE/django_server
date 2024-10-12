244a3c86d6949796aac15304d3a72334f46bdafa
dodni-dode <108711777+dodni-dode@users.noreply.github.com>
2024-10-12 14:37:30 +0900

    Delete ai_system directory

--- ai_system/__init__.py
+++
@@ -1,6 +0,0 @@
-# ai_system/__init__.py
-
-from .core import steps, factories
-from .core.config import Pipeline, Data, BaseConfig
-
-__all__ = ["Pipeline", "Data", "BaseConfig", "steps", "factories"]
--- ai_system/core/factories.py
+++
@@ -1,7 +0,0 @@
-from ..models.FaceDetector import FaceDetectorFactory
-from ..models.FacePredictor import FacePredictorFactory
-
-__all__ = [
-    "FaceDetectorFactory",
-    "FacePredictorFactory",
-]
